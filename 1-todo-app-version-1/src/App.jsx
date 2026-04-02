27ba78529908887871e26c2c2522827c97e397d7
Devendra Rajput <devenrajput12@gmail.com>
2026-04-02 20:19:53 +0530

    Initial Commit-Project

---
+++ 1-todo-app-version-1/src/App.jsx
@@ -0,0 +1,34 @@
+function App() {
+  return (
+    <center class="todo-container">
+      <h1>ToDo App</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter ToDo Here.."></input>
+          </div>
+          <div class="col-4">
+            <input type="date"></input>
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">
+              Add
+            </button>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6">Buy Milk..</div>
+          <div class="col-4">4/10/2026</div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
+}
+
+export default App;
