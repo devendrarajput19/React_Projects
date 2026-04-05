c1a375120e6da7eb04ad3098960b6d410ca41810
Devendra Rajput <devenrajput12@gmail.com>
2026-04-05 17:14:56 +0530

    Basic Program - Conditional Rendering

--- Basic Programs/Basic_Programs/src/App.jsx
+++ Basic Programs/Basic_Programs/src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+//
 function App() {
-  let footitem = ["Dal", "Green Vegetables", "Roti", "Salad"];
+  let footitem = [];
+  //let footitem = ["Dal", "Green Vegetables", "Roti", "Salad"];
 
+  // if (footitem.length === 0) {
+  //   return <h3> I am still hungry...</h3>;
+  // }
+  // Adding Ternary condition below
   return (
     <>
       <h1>Healthy Food...</h1>
+      {footitem.length === 0 ? <h3>I am still Hungry...</h3> : null}
       <ul className="list-group">
         {footitem.map((item) => (
           <li key={item} className="list-group-item">
@@ -18,7 +24,7 @@ function App() {
   );
 }
 
-// <> is called Fragment
+// <> is called Fragment. Only one data can be return, so either we add div and return value or we use fragment
 // map method is used to render list from array data. It works like loop.
 // key={item} that is the unique name we provide. In this case we have used our item name as unique value.
 
