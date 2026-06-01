94369101b6f90e6b88bb312c1fdae711e4071aea
Devendra Rajput <devenrajput12@gmail.com>
2026-06-02 00:01:03 +0530

    React Props - Part 3 - Spread and Rest Operator props

---
+++ Basic Programs/React Essentials/1 - React_Components_And_Props/React_props/src/UserCard.jsx
@@ -0,0 +1,37 @@
+// import { UserInfo } from "./UserInfo";
+
+// export const UserCard = ({ name, age, city, email }) => {
+//   return (
+//     <div>
+//       <h2>User Details</h2>
+//       <UserInfo name={name} age={age} city={city} email={email}></UserInfo>
+//     </div>
+//   );
+// };
+
+// Above code, we are repeating the props again, if there more than 10 props, than it wold be very clumsy code
+// So we are using it in below way
+
+import { UserInfo } from "./UserInfo";
+
+export const UserCard = (props) => {
+  return (
+    <div>
+      <h2>User Details</h2>
+      <UserInfo {...props}></UserInfo>
+    </div>
+  );
+};
+
+// This will forward all the props of UserInfo to UserCard. If there are any extra props then you can write in below way
+
+import { UserInfo } from "./UserInfo";
+
+export const UserCard = (id, ...rest) => {
+  return (
+    <div>
+      <h2>User {id} Details</h2>
+      <UserInfo {...rest}></UserInfo>
+    </div>
+  );
+};
