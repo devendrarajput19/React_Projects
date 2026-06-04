ee17f31cdfbe1ccda862a362d0dc53b8c6821001
Devendra Rajput <devenrajput12@gmail.com>
2026-06-04 23:51:20 +0530

    React Styling components - Part 3 - Css module structure

--- Basic Programs/React Essentials/1 - React_Components_And_Props/React_props/src/Alert.jsx
+++ Basic Programs/React Essentials/1 - React_Components_And_Props/React_props/src/Alert.jsx
@@ -1,11 +1,18 @@
-import "./Alert.css";
+import styles from "./Alert.module.css";
 
 // export const Alert = ({ children, type = "success" }) => {
 //   return <div className="alert">{children}</div>;
 // };
 
+// export const Alert = ({ children, type = "success" }) => {
+//   return <div className={`alert ${type}`}>{children}</div>;
+// };
+
+// Above way we can use class of alert and type both. This is the way to write. External css file
+
 export const Alert = ({ children, type = "success" }) => {
-  return <div className={`alert ${type}`}>{children}</div>;
+  return <div className={`${styles.alert} ${styles[type]}`}>{children}</div>;
 };
 
-// Above way we can use class of alert and type both. This is the way to write.
+// Above way we write for Css modules file. was css file are globally access. So We add css modules structure
+// [] are used for styles, as type is a dynamic value.
