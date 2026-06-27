067f672c25988dff23cc6b84362f70f134427876
Devendra Rajput <devenrajput12@gmail.com>
2026-06-27 23:44:33 +0530

    React State - useReducer function - 1

--- Basic Programs/React Essentials/2 - State-demo/src/App.jsx
+++ Basic Programs/React Essentials/2 - State-demo/src/App.jsx
@@ -7,12 +7,14 @@ import { PrevStateCounter } from "./PrevStateCounter";
 import { UserProfile } from "./UserProfile";
 import { ToDoList } from "./ToDoList";
 import { ShoppingCart } from "./ShoppingCart";
+import { CounterWithReducer } from "./CounterWithReducer";
 
 function App() {
   return (
     <>
       <h1>State-Demo</h1>
-      <ShoppingCart />
+      <CounterWithReducer />
+      {/* <ShoppingCart /> */}
       {/* <ToDoList /> */}
       {/* <UserProfile /> */}
       {/* <PrevStateCounter />
